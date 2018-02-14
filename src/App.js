13778dc3bb3a58d0ca7bbbffe0c911c58adc70dd
gregorh1 <grzegorz.hul@gmail.com>
2018-02-14 19:14:40 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Technologies from './Components/Technologies';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        MyApp
+          <Technologies/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Technologies.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Technologies extends Component {
+    render() {
+        return (
+            <div className="App">
+                MyTechnologies
+            </div>
+        );
+    }
+}
+
+export default Technologies;
